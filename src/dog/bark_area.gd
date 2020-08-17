c48666304a6b060c6e2b17d11cc37069b45a26b4
Cyril Bos <cbos.pro@gmail.com>
2020-08-17 23:09:59 +0200

    replaced bark area to manage a list of detected fams

--- src/dog/bark_area.gd
+++ src/dog/bark_area.gd
@@ -1,10 +1,11 @@
 extends Area
 
 export(float) var bark_duration = 0.25
-export(float) var bark_stun_duration = 2
 
 onready var _timer: Timer = $Timer
 onready var _woof_text: RichTextLabel = $WoofText
+
+var detected_fams = []
 
 func _input(event):
 	if event is InputEventKey and Input.is_action_just_pressed("bark"):
@@ -15,20 +16,24 @@ func _bark():
 	if not _timer.is_stopped():
 		print("cooldown")
 		return
-		
+	
 	# TODO: play sound
+	for fam in detected_fams:
+		fam.hears_bark()
 	_woof_text.display(get_global_transform().origin)
-	set_monitoring(true)
 	_timer.start(bark_duration)
 	print("bark!")
 
 
 func _on_BarkArea_body_entered(body):
-	print("%s detected" % body)
-	# actually not need but why not for safety
+	# actually not needed but why not for safety
 	if body is FamilyMember:
-		body.stun(bark_stun_duration)
+		detected_fams.append(body)
+		print("%s detected" % body)
 
 
-func _on_Timer_timeout():
-		set_monitoring(false)
+func _on_BarkArea_body_exited(body):
+	var body_index = detected_fams.find(body)
+	if body_index >= 0:
+		detected_fams.remove(body_index)
+		print("%s undetected" % body)
--- src/family_member.gd
+++ src/family_member.gd
@@ -2,6 +2,7 @@ extends KinematicBody
 class_name FamilyMember
 
 export(float) var speed: float = 3.0
+export(float) var bark_stun_duration = 2.0
 
 onready var _path : PathFollow = get_parent()
 onready var _stun_timer: Timer = $StunTimer
@@ -10,5 +11,5 @@ func _physics_process(delta: float):
 	if _stun_timer.is_stopped():
 		_path.set_offset(_path.get_offset() + speed * delta)
 
-func stun(duration: float):
-	_stun_timer.start(duration)
+func hears_bark():
+	_stun_timer.start(bark_stun_duration)
