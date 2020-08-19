07f9f3fdc2fce98c106b9d4c6068b229e37fd292
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-08-19 23:49:21 +0200

    Tweeked collision detection and animation. The code propably needs some refining\

--- src/objects/car.gd
+++ src/objects/car.gd
@@ -1,15 +1,17 @@
 extends KinematicBody
 
-export var speed = 1
+export var speed: int = 10
 
 onready var _path : PathFollow = get_parent()
 onready var _car_animplayer: AnimationPlayer = $Car/AnimationPlayer
+onready var _wait_time: Timer = $WaitTime
 
 
 var detected_fams_or_dog: Array = []
-var driveSpeed = 0
+var driveSpeed: int = 0
 
 var canDrive : int = 1
+var isWaiting:bool = false
 
 
 func _ready():
@@ -21,34 +23,37 @@ func _physics_process(delta):
 	else:
 		canDrive = 0
 
-	driveSpeed = lerp(driveSpeed, canDrive * speed, 0.05)
-
-	_path.set_offset(_path.get_offset() + driveSpeed * delta)	
+	if isWaiting == false:
+		driveSpeed = lerp(driveSpeed, canDrive * speed, 0.075)
+		
+		_path.set_offset(_path.get_offset() + 0.1 * driveSpeed * delta)	
 
 
 func _on_PeopleDetector_body_entered(body):
-	
-	if detected_fams_or_dog.size() <= 0:
-			_car_animplayer.play("Braking")
-			
-	# should not be needed but why not 
+
+		# should not be needed but why not 
 	if body is Dog or body is FamilyMember:
 		detected_fams_or_dog.append(body)
 		print("car %s detected %s" % [self, body])
-		
-		
-		
+
+	if detected_fams_or_dog.size() > 0:
+		if driveSpeed <= 0:
+			isWaiting = true
+		_car_animplayer.play("Braking")
+		print(isWaiting)
+
 
 
 func _on_PeopleDetector_body_exited(body):
-	if detected_fams_or_dog.size() <= 1:
-		_car_animplayer.play("SpeedingUp")
-	
-	detected_fams_or_dog.erase(body)
-	print("car %s undetected %s" % [self, body])
-	
 
+	if body is Dog or body is FamilyMember:
+		detected_fams_or_dog.erase(body)
+		print("car %s undetected %s" % [self, body])
 
+		if detected_fams_or_dog.size()  <= 0:
+			_car_animplayer.play("SpeedingUp")
+
+	
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	print("Animation " + anim_name + " finished")
@@ -56,9 +61,3 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	match anim_name:
 		"SpeedingUp": _car_animplayer.play("Driving")
 		"Braking": _car_animplayer.play("Honking")
-		
-	
-		
-
-	
-	pass # Replace with function body.
