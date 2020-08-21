e7fe8155c1d1eaf2e7c30ec6b31e5b4f450a88e6
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-08-21 22:57:35 +0200

    Added model of a child with animations and multiple textures

--- src/family/child.gd
+++ src/family/child.gd
@@ -5,12 +5,15 @@ var _speed: float = 6.0
 var _bark_stun_duration: float = 2.0
 
 onready var _stun_timer: Timer = $StunTimer
+onready var _anim_player: AnimationPlayer = $BOI/AnimationPlayer
 
 
 func _physics_process(delta: float):
 	if _stun_timer.is_stopped():
 		_move_on_path(_speed, delta)
+		_anim_player.play("Skip")
 
 
 func hears_bark():
 	_stun_timer.start(_bark_stun_duration)
+	_anim_player.play("Stun")
