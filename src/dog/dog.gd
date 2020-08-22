cca063fca23bdfa16c24efca25f7d39269cd6130
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-08-22 02:19:36 +0200

    Add Bark animation for dog. Dog reanimated, retextured (minor changes).

--- src/dog/dog.gd
+++ src/dog/dog.gd
@@ -2,28 +2,37 @@ extends KinematicBody
 class_name Dog
 
 export var speed = 10
-
+export var isBarking: bool = false
 
 var direction: Vector3 = Vector3()
 
 
-onready var _anim_tree: AnimationTree = $DogAnimated/AnimationTree
+onready var _anim_player: AnimationPlayer = $DogAnimated/AnimationPlayer
+onready var bark_timer: Timer = $BarkArea/Timer
 
+func _input(event):
+	if event is InputEventKey and Input.is_action_just_pressed("bark"):
+		isBarking = true
+		_anim_player.play("Bark")
 
-func _process(_delta):
+func _process(_delta):	
 	# reset the direction vector state
 	direction = Vector3()
 	
 	direction.z = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	direction.x = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
-
+	
+	
 
 func _physics_process(_delta):
-	if direction != Vector3.ZERO:
-		look_at(get_global_transform().origin + direction, Vector3.UP)
-		_anim_tree["parameters/movement/blend_amount"] = 1.0
-	elif direction == Vector3.ZERO:
-		_anim_tree["parameters/movement/blend_amount"] = 0.0
+	if isBarking:
+		direction = Vector3.ZERO
+	else:
+		if direction != Vector3.ZERO:
+			look_at(get_global_transform().origin + direction, Vector3.UP)
+			_anim_player.play("RunCycle")
+		elif direction == Vector3.ZERO:
+				_anim_player.play("Idle")
 		
 	move_and_slide(direction.normalized() * speed)
 
@@ -32,3 +41,9 @@ func _physics_process(_delta):
 func pet_the_dog():
 	# TODO
 	pass
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Bark":
+		isBarking = false
