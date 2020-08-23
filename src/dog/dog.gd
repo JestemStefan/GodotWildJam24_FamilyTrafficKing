aade5d4f00d5b4a4c569057035553bca525e0502
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-08-23 13:58:54 +0200

    Added particles for bark and dust for running

--- src/dog/dog.gd
+++ src/dog/dog.gd
@@ -9,6 +9,12 @@ var direction: Vector3 = Vector3()
 
 onready var _anim_player: AnimationPlayer = $DogAnimated/AnimationPlayer
 onready var _bark_area = $BarkArea
+onready var _waveParticle: Particles = $BarkArea/SoundWave
+onready var _soundSphere: Particles = $BarkArea/SoundSphere
+onready var _dust1: Particles = $Particles/Dust1
+onready var _dust2: Particles = $Particles/Dust2
+
+
 
 func _input(event):
 	if event is InputEventKey and Input.is_action_just_pressed("bark"):
@@ -16,6 +22,8 @@ func _input(event):
 		_bark_area.bark()
 		_anim_player.play("Bark")
 		_anim_player.set_speed_scale(1)
+		_waveParticle.set_emitting(true)
+		_soundSphere.set_emitting(true)
 
 
 func _process(_delta):
@@ -33,6 +41,8 @@ func _physics_process(_delta):
 		if direction != Vector3.ZERO:
 			look_at(get_global_transform().origin + direction, Vector3.UP)
 			_anim_player.play("RunCycle")
+			_dust1.set_emitting(true)
+			_dust2.set_emitting(true)
 			
 			#sync of animation and speed
 			_anim_player.set_speed_scale(speed/6.6)
@@ -40,6 +50,8 @@ func _physics_process(_delta):
 		elif direction == Vector3.ZERO:
 				_anim_player.play("Idle")
 				_anim_player.set_speed_scale(1.5)
+				_dust1.set_emitting(false)
+				_dust2.set_emitting(false)
 				
 
 		
