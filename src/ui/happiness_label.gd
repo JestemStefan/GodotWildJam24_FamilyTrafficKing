5db6184542dd1c5d61bf3e8819af0e66b90c5ed3
Cyril Bos <cbos.pro@gmail.com>
2020-08-23 14:43:19 +0200

    display current happines value and threshold

--- src/ui/happiness_label.gd
+++ src/ui/happiness_label.gd
@@ -3,7 +3,7 @@ extends Label
 
 func _ready():
 	HappinessManager.connect("happiness_updated", self, "_update_display")
-	
+	_update_display(0)
 
 func _update_display(happiness_val):
-	set_text("Happiness: " + str(happiness_val))
+	set_text("Happiness: %d (>= %d needed)" % [happiness_val, HappinessManager.get_happiness_threshold()])
